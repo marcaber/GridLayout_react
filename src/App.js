e7f37345d04515f70f632615acd07fabf387c26e
marcaber <mbehrens84@gmail.com>
2019-10-04 08:36:52 +0200

    Refactor: class in sfc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import MainContent from "./components/MainContent"
+import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
-class App extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="App">
-           <Navbar />
-           <MainContent />
-           <Sidebar />
-           <Footer />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainContent />
+      <Sidebar />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
